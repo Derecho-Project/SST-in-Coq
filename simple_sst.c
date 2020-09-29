ed94b6e38ebccb6e05d4a115c6ad2624e59a75ba
Matthew Milano <milano@cs.cornell.edu>
2020-09-29 07:54:15 -0700

    start of SST, sync with Lennart and Haobin for more progress

--- simple_sst.c
+++ simple_sst.c
@@ -8,19 +8,54 @@ struct row{
     SST_DTYPE cols[SST_NUM_COLS];
 };
 
-typedef struct row row;
+typedef struct row row_t;
 
 struct sst{
     int my_row;
     struct row rows[SST_NUM_ROWS];
 };
 
-typedef struct sst sst;
+typedef struct sst sst_t;
 
-void sync_sst(sst *_from, sst *_to){
-    row* from = _from->rows + _from->my_row;
-    row* to = _to->rows + _from->my_row;
-    memcpy(to->cols,from->cols,sizeof(row));
+void sync_sst(sst_t *_from, sst_t *_to){
+    row_t* from = _from->rows + _from->my_row;
+    row_t* to = _to->rows + _from->my_row;
+    memcpy(to->cols,from->cols,sizeof(row_t));
+}
+
+typedef char bool;
+
+typedef bool (*predicate_t)(sst_t*);
+typedef void (*trigger_t)(sst_t*);
+
+struct action{
+    predicate_t prediate;
+    trigger_t trigger;
+};
+
+typedef struct action action_t;
+
+struct replica{
+    sst_t sst;
+    action_t actions[];
+};
+
+typedef struct replica replica_t;
+
+replica_t* new_replica(replica_t* old, action_t *actions, size_t num_actions){
+    static int next_replica_id = 0;
+    replica_t* init = malloc(sizeof(replica_t) + (sizeof(action_t)*num_actions));
+    //init actions
+    for (size_t i = 0; i < num_actions; ++i) init->actions[i] = actions[i];
+    //init sst
+    if (old){
+	memcpy(&init->sst, &old->sst, sizeof(sst_t));
+    }
+    else {
+	bzero(&init->sst,sizeof(sst_t));
+    }
+    init->sst.my_row = next_replica_id++;
+    return init;
 }
 
 int main(){}
